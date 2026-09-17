ecddd181229672a89cc498965a0003f11738bbca
Luis Reyes <reyesjesus.jjj@gmail.com>
2026-09-16 21:55:24 -0500

    feat: Plantilla base del proyecto spring boot

---
+++ ProyectoHerramientas/src/main/java/Grupo4/ProyectoHerramientas/ProyectoHerramientasApplication.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoHerramientas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHerramientasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHerramientasApplication.class, args);
+	}
+
+}
---
+++ ProyectoHerramientas/src/test/java/Grupo4/ProyectoHerramientas/ProyectoHerramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoHerramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHerramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
